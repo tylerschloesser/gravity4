a7467b204f15485e688b9cff9e17ffb8fac2376b
Tyler Schloesser <tschloes@amazon.com>
2021-07-05 08:49:44 -0700

    refactor render

--- src/game.ts
+++ src/game.ts
@@ -24,26 +24,37 @@ export interface InitGame {
   pointer$: Observable<Pointer | null>
 }
 
+export interface RenderArgs {
+  context: CanvasRenderingContext2D
+  pointer: Pointer | null
+  size: CanvasSize
+}
+
+function render(args: RenderArgs) {
+  const { context, pointer, size } = args
+  const { w, h } = size
+  context.fillStyle = '#444'
+  context.fillRect(0, 0, w, h)
+
+  if (pointer) {
+    context.strokeStyle = 'white'
+    if (pointer.down) {
+      context.strokeStyle = 'blue'
+    }
+    context.beginPath()
+    context.arc(pointer.x, pointer.y, 50, 0, 2 * Math.PI)
+    context.stroke()
+  }
+}
+
 export function newGame(init: InitGame): Game {
   const { context, resize$, pointer$ } = init
 
   scheduled(
     combineLatest([pointer$, resize$]),
     animationFrameScheduler
-  ).subscribe(([pointer, canvasSize]) => {
-    const { w, h } = canvasSize
-    context.fillStyle = '#444'
-    context.fillRect(0, 0, w, h)
-
-    if (pointer) {
-      context.strokeStyle = 'white'
-      if (pointer.down) {
-        context.strokeStyle = 'blue'
-      }
-      context.beginPath()
-      context.arc(pointer.x, pointer.y, 50, 0, 2 * Math.PI)
-      context.stroke()
-    }
+  ).subscribe(([pointer, size]) => {
+    render({ context, pointer, size })
   })
 
   return {}
