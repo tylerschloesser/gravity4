a4269e31f3100a5b228d729a8517c1a62ea9258e
Tyler Schloesser <tschloes@amazon.com>
2021-07-05 07:36:24 -0700

    typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,24 @@
 import webpack from 'webpack'
 
 const config: webpack.Configuration = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js',
-  },
+  mode: 'development',
+	entry: './src/index.ts',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
 }
 
 export default config
