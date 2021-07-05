7c048ead7c6cf638cdc154b6d47e2bad26d7330f
Tyler Schloesser <tschloes@amazon.com>
2021-07-05 07:42:44 -0700

    resize

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 const canvas = document.querySelector<HTMLCanvasElement>('canvas')!
 
-const h = (canvas.height = window.innerHeight)
-const w = (canvas.width = window.innerWidth)
+let h = (canvas.height = window.innerHeight)
+let w = (canvas.width = window.innerWidth)
 
 const context = canvas.getContext('2d')!
 
 context.fillStyle = '#444'
 context.fillRect(0, 0, w, h)
+
+window.addEventListener('resize', () => {
+  h = canvas.height = window.innerHeight
+  w = canvas.width = window.innerWidth
+
+  context.fillStyle = '#444'
+  context.fillRect(0, 0, w, h)
+})
