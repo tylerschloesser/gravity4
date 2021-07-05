02a728b33bf8733fedc1049ba04b50aa5b7dc4b6
Tyler Schloesser <tschloes@amazon.com>
2021-07-05 07:37:07 -0700

    html webpack plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,26 @@
 import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const config: webpack.Configuration = {
   mode: 'development',
-	entry: './src/index.ts',
-	output: {
-		path: __dirname + '/dist',
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
+  entry: './src/index.ts',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [new HtmlWebpackPlugin()],
 }
 
 export default config
