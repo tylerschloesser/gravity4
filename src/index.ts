f9e07d9ae6de649377cefca3affb95e3bc2a701e
Tyler Schloesser <tschloes@amazon.com>
2021-07-05 07:41:49 -0700

    canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hi')
+const canvas = document.querySelector<HTMLCanvasElement>('canvas')!
+
+const h = (canvas.height = window.innerHeight)
+const w = (canvas.width = window.innerWidth)
+
+const context = canvas.getContext('2d')!
+
+context.fillStyle = '#444'
+context.fillRect(0, 0, w, h)
