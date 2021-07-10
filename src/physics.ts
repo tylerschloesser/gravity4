092fe35d646b4ce703d1163fc77b00c3d044a800
Tyler Schloesser <tschloes@amazon.com>
2021-07-10 07:57:41 -0700

    increase gravity

--- src/physics.ts
+++ src/physics.ts
@@ -107,7 +107,7 @@ async function newPhysicsBox2d(state: GameState) {
   const { b2BodyDef, b2_dynamicBody, b2PolygonShape, b2Vec2, b2World } = box2d
 
   // in metres per second squared
-  const gravity = new b2Vec2(0, 100)
+  const gravity = new b2Vec2(0, 200)
   const world = new b2World(gravity)
 
   let ballBody: Box2D.b2Body
