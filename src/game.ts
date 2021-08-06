2856005581ce0bd2e6ed86ad6ee09a760e6c8f3a
Tyler Schloesser <tschloes@amazon.com>
2021-08-06 08:07:57 -0700

    update initial camera angle

--- src/game.ts
+++ src/game.ts
@@ -30,8 +30,7 @@ export async function newGame(init: GameArgs): Promise<Game> {
     ball,
     boxes,
     circles,
-    // initial angle is counter clockwise 90 degrees, aka facing "up" the y axis
-    camera: { angle: Math.PI / 2, av: 0 },
+    camera: { angle: 0, av: 0 },
     speed: 0,
     gravity: vec2(),
     debug: {
